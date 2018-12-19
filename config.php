83c73bd6a0a7cd4cf17dce93a6c6a6beb55277c8
khunay89 <45991151+khunay89@users.noreply.github.com>
2018-12-19 08:20:48 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "866845037622850",
+"sign" => "74ad0d408dc039dfda268bc46031cb0c05c6721a",
+"token" => "489d621982b1ccf5339f32fe41add1f3",
+"uuid" => "2486b26b-f0b3-4bef-8b9e-b1874624a0b4"
+];
